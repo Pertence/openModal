83724eb0fdd2f3a1995da127c0f5e7412d41facc
Augusto Pertence <antonio.pertence@sga.pucminas.br>
2019-11-08 08:37:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,24 +20,16 @@ cd client
 npm install
 npm remove csstools/normalize.css
 npm install csstools/normalize.css
+npm install serve
 npm run build
-npm install -g serve
 
 #Instalação dos módulos python
 
 pip3 install matplotlib
-
 pip3 install networkx==2.3
-
 pip3 install sklearn
-
 pip3 install folium
-
 pip3 install pandas
-
 pip3 install plotly==3.10.0
-
 pip3 install shapely
-
 pip3 install geopy
-
